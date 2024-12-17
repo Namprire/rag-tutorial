2ea679090511899db8292abb97cf104621b341f3
Namprire <napham@outlook.de>
2024-12-17 16:08:19 +0900

    Refactor embedding function to use OllamaEmbeddings and update import paths

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
 
@@ -6,8 +6,8 @@ def get_embedding_function():
     #aws profile name and region name can be changed as per the user's aws configuration
     #change that to ollama
     #embeddings are important for the model to understand the context of the text
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
-    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    #embeddings = BedrockEmbeddings(
+    #    credentials_profile_name="default", region_name="us-east-1"
+    #)
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
